05a17a211c4edc632e95a6c66bed095294430896
Fanny <CodeWithPhany@github.com>
2021-12-06 13:56:59 +0100

    added read file as a standard, did part 1 & 2

--- days/6/lanternfishPt1.js
+++ days/6/lanternfishPt1.js
@@ -1,33 +1,31 @@
-const fs = require('fs');
-const path = require('path');
+const path   = require('path')
+const fs     = require('fs')
+const assert = require('assert')
 
-const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
-const dataset = fs.readFileSync(filePath).toString().split(",").map(x => parseInt(x, 10));
+const data = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8').split(',')
 
-function createFish(initialnumber) {
-    return { initialAge: initialnumber, currentAge: initialnumber };
+// part 1
+function part1(sequence) {
+  const arr = sequence.slice()
+  for (let i = 0; i < 80; i++) {
+    const length = arr.length
+    for (let j = 0; j < length; j++) {
+      if (0 === arr[j]) {
+        arr[j] = 6
+        arr.push(8)
+      } else {
+        arr[j]--
+      }
+    }
+  }
+
+  return arr.length
 }
 
-const fishes = input
-for (number of dataset) {
-    fishes.push(createFish(number));
-}
-console.log('Fishes at start: ' + fishes.length);
-function ageFish(fish, fishlist) {
-    if (fish.currentAge == 0) {
-        fish.currentAge = 6;
-        fishlist.push(createFish(8));
-    } else {
-        fish.currentAge--;
-    }
-}
-console.log('Initial state: ' + fishes.map(x => x.currentAge).join(','));
-for (let date = 0; date < 256; date++) {
-    for (fish of fishes.slice()) {
-        ageFish(fish, fishes);
-    }
-    if (date <= 5) {
-        console.log('After ' + (date + 1) + ' day:   ' + fishes.map(x => x.currentAge).join(','));
-    }
-}
-console.log('fishes at day 80: ' + fishes.length);
+const testData = [3,4,3,1,2]
+
+assert.equal(part1(testData), 5934)
+
+console.log(`
+Part 1: ${part1(data)}
+`)
